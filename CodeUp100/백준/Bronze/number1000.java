b11b7faa454a7b6d63af1dfbcd8654634a627c27
Kang-Seokjun <kangsj8011@gmail.com>
2023-02-04 21:15:37 +0900

    [Bronze V] 1000 A+B

---
+++ CodeUp100/백준/Bronze/number1000.java
@@ -0,0 +1,15 @@
+package Bronze;
+
+import java.util.Scanner; 
+public class number1000 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int firstNum = scanner.nextInt();
+		int secondNum = scanner.nextInt();
+		
+		System.out.println(firstNum+secondNum);
+	
+	}
+}
