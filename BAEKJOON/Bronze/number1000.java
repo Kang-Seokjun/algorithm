9842961df33dd179d4677f024bb75538bed2acbd
Kang-Seokjun <kangsj8011@gmail.com>
2023-02-04 21:41:37 +0900

    [Bronze V] 1000 A+B

---
+++ BAEKJOON/Bronze/number1000.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner; 
+public class number1000 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int firstNum = scanner.nextInt();
+		int secondNum = scanner.nextInt();
+		
+		System.out.println(firstNum+secondNum);
+	}
+}
